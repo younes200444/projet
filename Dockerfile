ec292f3dd98069fe2188eea58d8d16d5bbf63060
younes200444 <youneselamrani015@gmail.com>
2026-01-03 23:11:36 +0100

    ddd

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,16 @@
-FROM eclipse-temurin:21-jre
+FROM maven:3.9.6-eclipse-temurin-21 AS build
 
 WORKDIR /app
 
-COPY target/demo-0.0.1-SNAPSHOT.jar app.jar
+COPY pom.xml .
+RUN mvn dependency:go-offline
+COPY src ./src
+RUN mvn mvn clean package -DskipTests
+
+FROM eclipse-temurin:21-jre-alpine
+WORKDIR /app
+COPY --from=build /app/target/*.jar app.jar
 
 EXPOSE 8080
 
-ENTRYPOINT ["java", "-jar", "app.jar"]
+ENTRYPOINT ["java","-jar","app.jar"]
--- src/main/java/com/example/demo/controller/test.java
+++ src/main/java/com/example/demo/controller/test.java
@@ -4,13 +4,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
 @RequestMapping("/api")
 public class test {
 
     @GetMapping
-    public  String getDtring(){
-        return "hello Ilias el amrani";
+    public Map<String, String> message() {
+        return Map.of(
+                "from", "Spring Boot",
+                "content", "Bonjour depuis le backend 🚀"
+        );
     }
 
 }
